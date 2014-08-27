949ef373058476dd6316680e3167d4d222e91cdc
GiantCrocodile <iaa_1@web.de>
2014-08-28 00:45:03 +0200

    improved <head> file, added custom css function

--- app/views/head.blade.php
+++ app/views/head.blade.php
@@ -5,11 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ Config::get('mangoscms.head.title') }}</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-    @if (Config::get('mangoscms.site.customcss') == 1)
+    {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css') }}
+    @if (Config::get('mangoscms.site.customcss') === 1)
         {{ HTML::style('css/custom.css') }}
     @endif
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="//cdn.ckeditor.com/4.4.2/standard/ckeditor.js"></script>
+    {{ HTML::script('//code.jquery.com/jquery-1.11.1.min.js') }}
+    {{ HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js') }}
+    @if (Request::path() == '/dashboard')
+        {{ HTML::script('//cdn.ckeditor.com/4.4.2/standard/ckeditor.js') }}
+    @endif
 </head>
